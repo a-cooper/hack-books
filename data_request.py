0ee31c4c0d53eb3dba70c0a76a3912282c86f35b
A Cooper <anabelle.cooper16@gmail.com>
2020-10-17 17:58:53 +1000

    Author mapping complete.

--- data_request.py
+++ data_request.py
@@ -8,6 +8,8 @@ WIKIDATA_SEARCH = "https://www.wikidata.org/w/api.php?action=query&format=json&l
 WIKIDATA_PARSE = "https://www.wikidata.org/wiki/Special:EntityData/"
 GENDER_MAP_JSON = "reference_data/gender_mapping.json"
 COUNTRY_MAP_JSON = "reference_data/country_mapping.json"
+CONTINENT_MAP_JSON = "reference_data/continent_mapping.json"
+AUTHOR_MAP_JSON = "reference_data/author_mapping.json"
 
 NATIONALITY = "country of citizenship"
 GENDER = "sex or gender"
@@ -19,6 +21,9 @@ class ReferenceData():
 
         with open(COUNTRY_MAP_JSON) as f:
             self.country_map = json.load(f)
+
+        with open(AUTHOR_MAP_JSON) as f:
+            self.author_map = json.load(f)
 
     def get_gender(self, id: str):
         gender = self.gender_map.get(id)
@@ -38,9 +43,8 @@ class ReferenceData():
             country_name = data.get("labels").get("en").get("value")
             country["name"] = country_name
             region = data.get("claims").get("P361")[0].get("mainsnak").get("datavalue").get("value").get("id") # P361 is the 'part of' tag
-            region_name, continent_name = self.get_regions(region)
+            region_name = self.get_regions(region)
             country["region"] = region_name
-            country["continent"] = continent_name
             self.country_map[id] = country
             return country
 
@@ -48,10 +52,8 @@ class ReferenceData():
         region_data = json.loads(requests.get(WIKIDATA_PARSE + id + ".json").text)
         data = region_data.get("entities").get(id)
         region_name = data.get("labels").get("en").get("value")
-        continent_id = data.get("claims").get("P361")[0].get("mainsnak").get("datavalue").get("value").get("id") # P361 is the 'part of' tag
-        continent_data = json.loads(requests.get(WIKIDATA_PARSE + continent_id + ".json").text)
-        continent_name = continent_data.get("entities").get(continent_id).get("labels").get("en").get("value")
-        return region_name, continent_name
+        #continent_id = data.get("claims").get("P30")[0].get("mainsnak").get("datavalue").get("value").get("id")
+        return region_name #, self.continent_map.get(continent_id)
 
 
     def update_gender_map(self, key: str, value: str):
@@ -63,6 +65,8 @@ class ReferenceData():
             json.dump(self.gender_map, f)
         with open(COUNTRY_MAP_JSON, 'w') as f:
             json.dump(self.country_map, f)
+        with open(AUTHOR_MAP_JSON, 'w') as f:
+            json.dump(self.author_map, f)
 
 def test_request():
     response = requests.get("https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=Akwaeke%20Emezi&format=json")
@@ -81,27 +85,34 @@ def get_page_id(text: str):
     return top_page.get("title")
 
 
-def get_page_info(pageid: str):
+def get_author_info(author: str):
+    author_details = reference.author_map.get(author)
+    if author_details:
+        return author_details
+    pageid = get_page_id(author)
     response = requests.get(WIKIDATA_PARSE + pageid + ".json")
     data = json.loads(response.text)
     properties = data.get("entities").get(pageid).get("claims")
-    get_gender(properties)
-    get_nationality(properties)
+    author_details = {"id": pageid, "gender": get_gender(properties)}
+    country_details = get_nationality(properties)
+    author_details["country"] = country_details.get("name")
+    author_details["region"] = country_details.get("region")
+    reference.author_map[author] = author_details
+    return author_details
 
 #def extract_pageid()
 
 def get_gender(data: dict):
     gender = data.get("P21")[0].get("mainsnak").get("datavalue").get("value").get("id")
-    print(reference.get_gender(gender))
+    return reference.get_gender(gender)
 
 
 def get_nationality(data: dict):
-    nationality = data.get("P27")[0].get("mainsnak").get("datavalue").get("value").get("id")
-    print(reference.get_country(nationality))
+    country = data.get("P27")[0].get("mainsnak").get("datavalue").get("value").get("id")
+    return reference.get_country(country)
 
 reference = ReferenceData()
-id = get_page_id("Chimamanda Ngozi Adichie")
-get_page_info(id)
+get_author_info("Jules Verne")
 reference.update_maps_jsons()
 
 
