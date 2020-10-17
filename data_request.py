d626bc1b175c786b3085dd8963d8dc2eb4ee8110
A Cooper <anabelle.cooper16@gmail.com>
2020-10-17 12:21:44 +1000

    Can parse json for gender and country info ids

--- data_request.py
+++ data_request.py
@@ -2,13 +2,44 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-import re
+from html.parser import HTMLParser
 
 WIKIDATA_SEARCH = "https://www.wikidata.org/w/api.php?action=query&format=json&list=search"
-WIKIDATA_PARSE = "https://www.wikidata.org/w/api.php?action=parse"
+WIKIDATA_PARSE = "https://www.wikidata.org/wiki/Special:EntityData/"
+GENDER_MAP_JSON = "reference_data/gender_mapping.json"
+COUNTRY_MAP_JSON = "reference_data/country_mapping.json"
 
 NATIONALITY = "country of citizenship"
 GENDER = "sex or gender"
+
+class ReferenceData():
+    def __init__(self):
+        with open(GENDER_MAP_JSON) as f:
+            self.gender_map = json.load(f)
+
+        with open(COUNTRY_MAP_JSON) as f:
+            self.country_map = json.load(f)
+
+    def get_gender(self, id: str):
+        gender = self.gender_map.get(id)
+        if gender:
+            return gender
+        # TODO have it look up the value
+        return "Unknown"
+
+    def get_country(self, id: str):
+        country = self.country_map.get(id)
+        if country:
+            return country
+        # TODO have it look up the value
+        return "Unknown"
+
+    def update_gender_map(self, key: str, value: str):
+        self.gender_map.put(key, value)
+        with open('reference_data/gender_mapping.json', 'w') as f:
+            json.dump(GENDER_MAP_JSON, f)
+
+
 
 def test_request():
     response = requests.get("https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=Akwaeke%20Emezi&format=json")
@@ -28,22 +59,25 @@ def get_page_id(text: str):
 
 
 def get_page_info(pageid: str):
-    response = requests.get(WIKIDATA_PARSE + "&page=" + pageid)
-    soup = BeautifulSoup(response.text, 'lxml')
-    get_gender(response.text)
-    #print(soup.body.div)
+    response = requests.get(WIKIDATA_PARSE + pageid + ".json")
+    data = json.loads(response.text)
+    properties = data.get("entities").get(pageid).get("claims")
+    get_gender(properties)
+    get_nationality(properties)
 
 #def extract_pageid()
 
-def get_gender(data: str):
-    start = "<a title=\"Property:P21\" href=\"/wiki/Property:P21\">sex or gender</a>"
-    end = "<<a title=\"Property:\">"
-    result = re.findall(r".*", data)
-    #data = soup.find_all(string=re.compile('P21'))
-    print(result)
+def get_gender(data: dict):
+    gender = data.get("P21")[0].get("mainsnak").get("datavalue").get("value").get("id")
+    print(reference.get_gender(gender))
 
 
+def get_nationality(data: dict):
+    nationality = data.get("P27")[0].get("mainsnak").get("datavalue").get("value").get("id")
+    print(reference.get_country(nationality))
 
+reference = ReferenceData()
 id = get_page_id("Akwaeke Emezi")
 get_page_info(id)
+
 
